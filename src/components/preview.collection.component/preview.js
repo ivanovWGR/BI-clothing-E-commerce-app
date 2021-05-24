8e981f23cefbc54072475e418960eb6a13af6432
Borislav Ivanov <borislavivanovtw@abv.bg>
2021-05-24 17:52:51 +0300

    add all preview categories for new shop page

--- src/components/preview.collection.component/preview.js
+++ src/components/preview.collection.component/preview.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./preview.styles.scss";
+import CollectionItem from '../collection-item/collection-item';
 
 const PREVIEW_COLLECTION = ({ title, items }) => {
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
-        {items.filter((item,i)=>i<4).map((item) => (
-          <div key={item.id}>{item.name} </div>
-        ))}
+        {items
+          .filter((item, i) => i < 4)
+          .map((item) => (
+            <CollectionItem {...item} key={item.id}/>
+          ))}
       </div>
     </div>
   );
