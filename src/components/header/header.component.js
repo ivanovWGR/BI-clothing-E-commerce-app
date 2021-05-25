3da44d5b38cf80e5a64a8b7a5c99b28dab901ff8
Borislav Ivanov <borislavivanovtw@abv.bg>
2021-05-26 01:22:53 +0300

    adding route,update header component

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,16 +1,25 @@
 import React from "react";
 import "./header.component.styles.scss";
-import {Link} from 'react-router-dom';
-import {ReactComponent as Logo} from '../../assets/crown.svg'; //import svg as Logo from assets
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg"; //import svg as Logo from assets
 
-const headerComponent = () => {
+const Header = () => {
   return (
-    <div>
-     <Link to="/">
-       <Logo/>
-     </Link>
+    <div className="header">
+      <Link to="/" className="logo-container">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+
+        <Link className="option" to="/contact">
+          CONTACT
+        </Link>
+      </div>
     </div>
   );
 };
 
-export default headerComponent;
+export default Header;
