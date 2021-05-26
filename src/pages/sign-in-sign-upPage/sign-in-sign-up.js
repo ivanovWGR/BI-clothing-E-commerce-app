d8e338632d5d93d8454df60af72a9b3400b0fd6a
Borislav Ivanov <borislavivanovtw@abv.bg>
2021-05-26 15:42:59 +0300

    test

--- src/pages/sign-in-sign-upPage/sign-in-sign-up.js
+++ src/pages/sign-in-sign-upPage/sign-in-sign-up.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 import "./sign-in-sign-up.styles.scss";
+import SignIn from "../../components/signin.component/signin.component";
 
-const  SignInSignUpPage = () => {
-    return (
-        <div className="sign-in-sign-up">
-          SIGN IN
-        </div>
-    )
-}
+const SignInSignUpPage = () => {
+  return (
+    <div className="sign-in-sign-up">
+      <SignIn />
+    </div>
+  );
+};
 
 export default SignInSignUpPage;
